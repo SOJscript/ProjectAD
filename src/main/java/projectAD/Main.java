2830bb204a257d0b5760fd58507744d3e7853e61
SOJscript <sergioliverfeina@gmail.com>
2026-05-18 10:46:06 +0200

    prueba sergi

--- src/main/java/projectAD/Main.java
+++ src/main/java/projectAD/Main.java
@@ -1,5 +1,12 @@
 package projectAD;
 
 public class Main {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        menuDB();
+    }
+
+    public static void menuDB(){
+
+    }
 }
+
