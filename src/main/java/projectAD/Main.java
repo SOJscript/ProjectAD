885d7cc17af153199c1088d311ede5e6d5e3d8a2
SOJscript <sergioliverfeina@gmail.com>
2026-05-18 10:50:18 +0200

    prueba

--- src/main/java/projectAD/Main.java
+++ src/main/java/projectAD/Main.java
@@ -2,11 +2,7 @@ package projectAD;
 
 public class Main {
     public static void main(String[] args) {
-        menuDB();
-    }
-
-    public static void menuDB(){
-
+        int papaya;
     }
 }
 
