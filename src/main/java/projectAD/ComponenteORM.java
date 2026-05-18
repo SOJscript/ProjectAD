2a4df0c556cff514621dac25aede7e00737483a1
SOJscript <sergioliverfeina@gmail.com>
2026-05-18 11:38:27 +0200

    Creacion de las clases Componente para cada BD

---
+++ src/main/java/projectAD/ComponenteORM.java
@@ -0,0 +1,60 @@
+package projectAD;
+
+import java.util.List;
+
+public class ComponenteORM implements IDAO {
+    @Override
+    public List<Employee> findAllEmployees() {
+        return List.of();
+    }
+
+    @Override
+    public Employee findEmployeeById(Object id) {
+        return null;
+    }
+
+    @Override
+    public boolean addEmployee(Employee employee) {
+        return false;
+    }
+
+    @Override
+    public boolean updateEmployee(Object id) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteEmployee(Object id) {
+        return false;
+    }
+
+    @Override
+    public List<Department> findAllDepartments() {
+        return List.of();
+    }
+
+    @Override
+    public Department findDepartmentById(Object id) {
+        return null;
+    }
+
+    @Override
+    public boolean addDepartment(Department department) {
+        return false;
+    }
+
+    @Override
+    public boolean updateDepartment(Object id) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteDepartment(Object id) {
+        return false;
+    }
+
+    @Override
+    public List<Employee> findEmployeesByDept(Object idDept) {
+        return List.of();
+    }
+}
